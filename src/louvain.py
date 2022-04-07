2e1be4607ac50a3fd76f5cbd47dcb1305f2db15b
dmaric44 <danielmaric44@gmail.com>
2022-04-07 20:17:05 +0200

    testing evaluation methods

--- src/louvain.py
+++ src/louvain.py
@@ -6,62 +6,69 @@ import matplotlib.pyplot as plt
 
 import util
 
+def evaluate(algorithm, graph, communities, coms):
+    print(algorithm)
+    mod = nx.algorithms.community.modularity(graph, communities)
+    print(mod)
+
+    mod = evaluation.surprise(graph, coms)
+    print(mod)
+
+    scd = evaluation.avg_transitivity(graph, coms)
+    print(scd)
+
+    size = evaluation.size(graph, coms)
+    print(size)
+
+    triangles = evaluation.triangle_participation_ratio(graph, coms)
+    print(triangles)
+
+    conductance = evaluation.conductance(graph, coms)
+    print(conductance)
+
+    print()
+
+
 if __name__ == '__main__':
     # G = nx.generators.watts_strogatz_graph(1000, 20, 0.1)
-    G1 = util.loadDataFromSNAP(r"C:\FER\10. semestar\Diplomski rad\community-detection\SNAP_data\facebook_combined.txt")
+    G1 = util.loadDataFromSNAP(r"C:\FER\9. semestar\Diplomski projekt\py_code\test_SNAP\SNAP_data\facebook_combined.txt")
     G = util.convertSnapToNx(G1)
 
+    print("Louvain:")
     coms = algorithms.louvain(G, weight='weight', resolution=1.)
     communities = []
     for i in coms.communities:
-        print(i)
+        # print(i)
         communities.append(i)
+    evaluate("Louvain", G, communities, coms)
 
-    mod = nx.algorithms.community.modularity(G,communities)
-    print(mod)
 
-    mod = evaluation.surprise(G, coms)
-    print(mod)
-    print()
-
+    print("surprise:")
     coms = algorithms.surprise_communities(G)
     communities = []
     for i in coms.communities:
-        print(i)
+        # print(i)
         communities.append(i)
 
-    mod = nx.algorithms.community.modularity(G, communities)
-    print(mod)
+    evaluate("surprise", G, communities, coms)
 
-    mod = evaluation.surprise(G, coms)
-    print(mod)
-    print()
-
+    print("Leiden:")
     coms = algorithms.leiden(G)
     communities = []
     for i in coms.communities:
-        print(i)
+        # print(i)
         communities.append(i)
+    evaluate("Leiden", G, communities, coms)
 
-    mod = nx.algorithms.community.modularity(G, communities)
-    print(mod)
-
-    mod = evaluation.surprise(G, coms)
-    print(mod)
-    print()
-
+    print("walktrap:")
     coms = algorithms.walktrap(G)
     communities = []
     for i in coms.communities:
-        print(i)
+        # print(i)
         communities.append(i)
+    evaluate("walktrap", G, communities, coms)
 
-    mod = nx.algorithms.community.modularity(G, communities)
-    print(mod)
 
-    mod = evaluation.surprise(G, coms)
-    print(mod)
-    print()
 
     # Girvan-Newman
     # G = util.convertNxToSnap(G)
